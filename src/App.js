c2332b3f01e1fb3f38a94209f87e2ee55d329633
Farhan <farhanshokat@gmail.com>
2023-04-04 17:48:41 +0500

    Top Header Setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./components/Header/Header";
-import Content from './components/Content/Content';
+// import Content from './components/Content/Content';
 
 
 function App() {
   return (
     <div>
       <Header />
-      <Content />
+      {/* <Content /> */}
     </div>
   );
 }
