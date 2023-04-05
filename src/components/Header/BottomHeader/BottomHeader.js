84f3bbd9cf9f19de8b698f0fc751651671d8310e
sajeel45 <sheikhsajeel0@gmail.com>
2023-04-05 16:29:50 +0500

    Header Spacing Commit

--- src/components/Header/BottomHeader/BottomHeader.js
+++ src/components/Header/BottomHeader/BottomHeader.js
@@ -23,30 +23,30 @@ const BottomHeader = () => {
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className=' nav-link'>
-                            <NavDropdown href="/home" title="Home" id='basic-nav-dropdown' menuVariant='dark'>
+                            <NavDropdown href="/home" title="Home" id='basic-nav-dropdown' menuVariant='dark' className='nav-spacing'>
                                 <NavDropdown.Item href='#' className='nav-link'>Home 1</NavDropdown.Item>
                                 <NavDropdown.Item href='#' className='nav-link'>Home 2</NavDropdown.Item>
                             </NavDropdown>
-                            <Nav.Link href='#' >Who We Are</Nav.Link>
-                            <NavDropdown title="Practice Areas" id='basic-nav-dropdown' menuVariant='dark'>
+                            <Nav.Link href='#' className='nav-spacing'>Who We Are</Nav.Link>
+                            <NavDropdown title="Practice Areas" id='basic-nav-dropdown' menuVariant='dark' className='nav-spacing'>
                                 <NavDropdown.Item href='#' className='nav-link'>2 Column Layout</NavDropdown.Item>
                                 <NavDropdown.Item href='#' className='nav-link'>3 Column Layout</NavDropdown.Item>
                                 <NavDropdown.Item href='#' className='nav-link'>Individual Practice Area</NavDropdown.Item>
                             </NavDropdown>
-                            <NavDropdown title='Team Members' id='basic-nav-dropdown' menuVariant='dark' >
+                            <NavDropdown title='Team Members' id='basic-nav-dropdown' menuVariant='dark' className='nav-spacing'>
                                 <NavDropdown.Item href='#' className='nav-link'>Robert Darren</NavDropdown.Item>
                             </NavDropdown>
-                            <NavDropdown title='Pages' id='basic-nav-dropdown' menuVariant='dark'>
+                            <NavDropdown title='Pages' id='basic-nav-dropdown' menuVariant='dark' className='nav-spacing'>
                                 <NavDropdown.Item href='#' className='nav-link'>FAQs</NavDropdown.Item>
                                 <NavDropdown.Item href='#' className='nav-link'>404 Page</NavDropdown.Item>
                             </NavDropdown>
-                            <NavDropdown title='Blogs' id = 'basic-nav-dropwdown' menuVariant='dark'>
+                            <NavDropdown title='Blogs' id = 'basic-nav-dropwdown' menuVariant='dark' className='nav-spacing'>
                                 <NavDropdown.Item href='#' className='nav-link'>Blog With SideBar</NavDropdown.Item>
                                 <NavDropdown.Item href='#' className='nav-link'>Blog FullPage</NavDropdown.Item>
                                 <NavDropdown.Item href='#' className='nav-link'>Blog Chess</NavDropdown.Item>
                                 <NavDropdown.Item href='#' className='nav-link'>Blog Grid</NavDropdown.Item>
                             </NavDropdown>
-                            <Nav.Link href='#'>Contact</Nav.Link>
+                            <Nav.Link href='#' className='nav-spacing'>Contact</Nav.Link>
                             </Nav>
                             </Navbar.Collapse>
 
