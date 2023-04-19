d746534cad3aec05e05fac181d28aaa0ba4f6111
sajeel45 <sheikhsajeel0@gmail.com>
2023-04-19 16:39:47 +0500

    Animation Setup

--- src/components/Achievements/Achievements.js
+++ src/components/Achievements/Achievements.js
@@ -2,33 +2,35 @@ import { Container, Row, Col } from "react-bootstrap";
 import { FaUser } from "react-icons/fa";
 import { FaBriefcase } from "react-icons/fa";
 import { FaTrophy } from "react-icons/fa";
+import "animate.css/animate.min.css";
+import { AnimationOnScroll } from "react-animation-on-scroll";
 import "./Achievements.css";
 
 const achievements = [
   {
     id: 1,
-    logo:<FaUser color='#63CAB5' className='icon-styling' size='60px'/>,
-    alt:'logo',
+    logo: <FaUser color="#63CAB5" className="icon-styling" size="60px" />,
+    alt: "logo",
     title: "20+ Years of Experience",
     description:
       "We are in the law industry for more than 20 years with more than 98% of client satisfaction",
   },
   {
     id: 2,
-    logo:<FaBriefcase color='#63CAB5' className='icon-styling' size='60px'/>,
-    alt:'logo',
+    logo: <FaBriefcase color="#63CAB5" className="icon-styling" size="60px" />,
+    alt: "logo",
     title: "Qualified And Experienced Attorneys",
     description:
       "Our team is the best in the business. We are team of attorneys which are passionate about their jobs.",
   },
   {
     id: 3,
-    logo:<FaTrophy color='#63CAB5' className='icon-styling' size='60px'/>,
-    alt:'logo',
+    logo: <FaTrophy color="#63CAB5" className="icon-styling" size="60px" />,
+    alt: "logo",
     title: "Awards And Achievements",
     description:
       "We at lawyero earned the best law firm award in 2017 with a success ratio of 99%.",
-  }
+  },
 ];
 
 const Achievements = () => {
@@ -36,29 +38,43 @@ const Achievements = () => {
     <>
       <Container className="overlap-medium panel-grid">
         <Row>
-            {achievements.map((item, index) => (
-              <Col lg='4' sm='12' md='6' className="achievement-wrapper" key={index}>
-                <div className="op-why-choose-section-style-2">
-                <div className="op-why-choose">
-
-                <div className="icon-wrapper">
-                  <span>
-                    {item.logo}
-                  </span>
-                </div>
-                <div className="title-wrapper">
-                  <h4>{item.title}</h4>
-                </div>
-                <div className="description-wrapper">
-                  <p>{item.description}</p>
-                </div>
-              </div>
-              </div>
-              </Col>
-            ))}
+          {achievements.map((item, index) => {
+            let animation = "";
+            if (index === 0 || index === 2) {
+              animation = "fadeUp";
+            } else if (index === 1) {
+              animation = "fadeDown";
+            }
+            return (
+              <AnimationOnScroll key={item.id} animateIn={animation}>
+                <Col
+                  lg="4"
+                  sm="12"
+                  md="6"
+                  className="achievement-wrapper"
+                  key={index}
+                >
+                  <div className="op-why-choose-section-style-2">
+                    <div className="op-why-choose">
+                      <div className="icon-wrapper">
+                        <span>{item.logo}</span>
+                      </div>
+                      <div className="title-wrapper">
+                        <h4>{item.title}</h4>
+                      </div>
+                      <div className="description-wrapper">
+                        <p>{item.description}</p>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+              </AnimationOnScroll>
+            );
+          })}
         </Row>
       </Container>
     </>
   );
 };
+
 export default Achievements;
