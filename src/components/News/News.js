7a526cffd62bf067577755f073befedff8e9e7ea
sajeel45 <sheikhsajeel0@gmail.com>
2023-05-01 19:26:30 +0500

    Minor Commit

---
+++ src/components/News/News.js
@@ -0,0 +1,19 @@
+import './News.css';
+
+
+const News = () => {
+    return(
+        <>
+        <div className='news-wrapper'>
+            <div className='news-heading-wrapper'></div>
+            <div className='news-main-wrapper'>
+                <div className='news-item-wrapper'>
+                    <div className='news-image-wrapper'></div>
+                    <div className='news-content-wrapper'></div>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+export default News;
