5701a562e6bf18ff436d8e133053cc8d095c4439
sajeel45 <sheikhsajeel0@gmail.com>
2023-06-08 18:37:06 +0500

    Composition Commit

---
+++ src/components/BannerTest/BannerTest.js
@@ -0,0 +1,50 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./BannerTest.css";
+import {Container,Row} from "react-bootstrap";
+
+
+const settings = {
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  adaptiveHeight: true
+};
+
+const Banner = (props) => {
+
+  const bannerImages = props.bannerImages;
+
+  return (
+    <Row>
+
+      {console.log(bannerImages)}
+
+    <section className="op-slogan-section op-title-wrapper dark-color-overlay-over-image">
+        <div className="page-content-container">
+          <Slider {...settings} autoplay="true" autoplaySpeed={3000} className="op-banner">
+            {bannerImages.map((item, index)=>(
+            <div className="item" key={index}>
+              <img src={item.src} alt="banner" />
+                <div className="carousel-caption">
+                <Container>
+                  <h2 className="h1style">{item.heading}</h2>
+                  <a
+                    className="primary-button button-default"
+                    href={item.link}
+                  >
+                    {item.caption}
+                    <span>{item.icon}</span>
+                  </a>
+                  </Container>
+                </div>
+            </div>
+            ))}
+          </Slider>
+        </div>
+    </section>
+    </Row>
+  );
+};
+export default Banner;
---
+++ src/components/BannerWrapper/BannerWrapper.js
@@ -0,0 +1,8 @@
+
+
+const BannerContainer = ({ children }) => {
+    return <div className="banner-container">{children}</div>;
+  };
+  
+  export default BannerContainer;
+  
