fb08b6998a4f118c74ed436f2b11a2e900558479
sajeel45 <sheikhsajeel0@gmail.com>
2023-06-09 17:14:35 +0500

    Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header/Header";
-import Content from './components/Content/Content';
-
-
-
-
+import Content from "./components/Content/Content";
+import React from "react";
+import { ReactDOM } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
 function App() {
-  
-  
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <App />
+  //   },
+  //   {
+  //     path: "/test",
+  //     element: <Header />,
+  //   },
+  // ]);
+  // const router = createBrowserRouter(
+  //   createRoutesFromElements(
+  //     <Route path="/" element={<App />}>
+  //       <Route path="test" element={<Header />} />
+  //     </Route>
+  //   )
+  // );
+
+  // const router = createBrowserRouter([
+  //   {
+  //     path:'/',
+  //     element:(
+  //       <div>
+  //       <h1>Hello World</h1>
+  //       <Link to="about">About Us</Link>
+  //     </div>
+  //     ),
+  //   },
+  //   {
+  //     path: "about",
+  //     element: <Header />,
+  //   },
+  // ]);
+  // createRoot(document.getElementById("root")).render(
+  //   <RouterProvider router={router} />
+  // );
   return (
     <div>
       <Header />
-      <Content/>
+      <Content />
     </div>
   );
 }
 
 export default App;
-
--- src/components/Header/TopHeader/TopHeader.js
+++ src/components/Header/TopHeader/TopHeader.js
@@ -1,6 +1,5 @@
-import Container from "react-bootstrap/Container";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import {Container,Row,Col} from "react-bootstrap";
+import { Link } from 'react-router-dom';
 import "./TopHeader.css";
 
 const TopHeader = () => {
@@ -9,7 +8,7 @@ const TopHeader = () => {
             <Container>
                 <Row>
                     <Col xs={12} sm={6} className='op-call-us text-start'>
-                        Call Us: <a className="tel" href="tel:1911462242">1911-462-242</a>
+                        Call Us: <a className="tel" href="tel:1911-462-242">1911-462-242</a>
                     </Col>
                     <Col xs={12} sm={6} className='op-request text-end bold'>
                         {/* <Link to={""}>Request a free consultation</Link> */}
